b697c0920462a59719430888c15d437452e277de
Lheanarda <alexanderleo0499@gmail.com>
2020-07-19 01:58:11 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularQuiz06';
+
+  @ViewChild('quizform',{static:true}) quizForm:NgForm;
+  default_sub = "advance";
+  submitted:boolean = false;
+
+  content={
+    email:'',
+    subscription:'',
+    password:''
+  }
+
+
+  onSubmit(form:NgForm){
+    console.log(form);
+    this.content.email = this.quizForm.value.email;
+    this.content.subscription = this.quizForm.value.subscription;
+    this.content.password= this.quizForm.value.password;
+
+    this.submitted = true;
+  }
 }
